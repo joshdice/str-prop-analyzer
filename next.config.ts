645b0309777fe4bcc99d5735ca05c395388d72a9
joshdice <joshdice@gmail.com>
2025-04-19 07:11:53 -0500

    missed two files from the github pages commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static exports
+  basePath: process.env.NODE_ENV === 'production' ? "/str-prop-analyzer" : "",
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
